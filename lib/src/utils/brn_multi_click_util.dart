5991726d660773287ab23f2df0b6311cfa43ee96
laiiihz <laiiihz@outlook.com>
2021-12-16 16:23:47 +0800

    migrate brn_multi_click_util to null safety

--- lib/src/utils/brn_multi_click_util.dart
+++ lib/src/utils/brn_multi_click_util.dart
@@ -1,12 +1,10 @@
-// @dart=2.9
-
 class BrnMultiClickUtils {
-  static DateTime _lastClickTime;
+  static DateTime? _lastClickTime;
 
-  static bool isMultiClick({int intervalMilliseconds}) {
+  static bool isMultiClick({int intervalMilliseconds = 500}) {
     if (_lastClickTime == null ||
-        DateTime.now().difference(_lastClickTime) >
-            Duration(milliseconds: intervalMilliseconds ?? 500)) {
+        DateTime.now().difference(_lastClickTime!) >
+            Duration(milliseconds: intervalMilliseconds)) {
       _lastClickTime = DateTime.now();
       return false;
     }
