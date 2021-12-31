c854ab290bfd164ed9faea0be13fa6ecd5881125
Sandy <15143015732@163.com>
2021-12-31 22:49:24 +0800

    优化 EventBus  单例实现 (#43)
    * Theme: 优化单例实现，初步优化 BrnAllThemeConfig 属性获取不为 null
    
    * refactor:优化theme,暴露非null引用
    
    * theme:去除无用信息及优化部分代码
    
    * theme:增加属性类型
    
    * 优化修复 EventBus

--- lib/src/utils/brn_event_bus.dart
+++ lib/src/utils/brn_event_bus.dart
@@ -32,16 +32,15 @@ class EventBus {
 
   static EventBus? _instance;
 
-  factory EventBus.init() {
-    _instance = EventBus();
+  factory EventBus._() {
+    if(_instance == null) {
+      _instance = EventBus();
+    }
     return _instance!;
   }
 
   static EventBus get instance {
-    if (_instance == null) {
-      EventBus.init();
-    }
-    return _instance!;
+    return EventBus._();
   }
 
   StreamController _streamController;
@@ -64,7 +63,7 @@ class EventBus {
   /// resumed or cancelled. So it's usually better to just cancel and later
   /// subscribe again (avoids memory leak).
   ///
-  Stream on<T>() {
+  Stream<T> on<T>() {
     if (T == dynamic) {
       return streamController.stream as Stream<T>;
     }
@@ -80,19 +79,5 @@ class EventBus {
   void destroy() {
     _streamController.close();
   }
-  static EventBus? _instance;
 
-  factory EventBus.init() {
-    if (_instance == null) {
-      _instance = EventBus();
-    }
-    return _instance!;
-  }
-
-  static EventBus get instance {
-    if (_instance == null) {
-      EventBus.init();
-    }
-    return _instance!;
-  }
 }
