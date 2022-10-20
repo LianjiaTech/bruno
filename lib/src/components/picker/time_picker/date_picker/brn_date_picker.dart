ccea5b237bf7a2a36d9145210b1a93335efeffa9
zhoujuanjuan <15143015732@163.com>
2022-10-20 18:10:55 +0800

    fix showDatePicker notes

--- lib/src/components/picker/time_picker/date_picker/brn_date_picker.dart
+++ lib/src/components/picker/time_picker/date_picker/brn_date_picker.dart
@@ -71,7 +71,7 @@ class BrnDatePicker {
     /// 点击【取消】回调给调用方的回调事件
     DateVoidCallback? onCancel,
 
-    /// 点击【关闭】回调给调用方的数据
+    /// 弹框点击外围消失的回调事件
     DateVoidCallback? onClose,
 
     /// 时间滚动选择时候的回调事件
